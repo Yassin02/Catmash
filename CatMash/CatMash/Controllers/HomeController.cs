0d7e7f269d6a6111b8dc788fb3a65c82758bcfbb
Yassine FASTANI <fastani.yassine@gmail.com>
2018-11-03 16:04:33 +0100

    Add View and Controller in order to show cat images to compare (In progress)

---
+++ CatMash/CatMash/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using CatMash.Services;
+using System.Web.Mvc;
+
+namespace CatMash.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.List = CatComparerService.CatsToCompare();
+
+            return View();
+        }
+    }
+}
